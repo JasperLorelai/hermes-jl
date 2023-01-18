5f5035bc8a888699560d1e570442b2b3c16760e8
JasperLorelai <wolf.haci12@gmail.com>
2023-01-18 20:01:57 +0100

    Add starter page instead of 404

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Page() {
+    return (
+        <div className="vh-100 d-flex align-items-center justify-content-center display-6">
+            There will be a page here someday. 😛
+        </div>
+    );
+}
