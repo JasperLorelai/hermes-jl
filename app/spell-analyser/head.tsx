01547bc9d4c890ff732494ab4a8a793144d19676
JasperLorelai <wolf.haci12@gmail.com>
2023-02-28 00:20:17 +0100

    Add meta

--- app/spell-analyser/head.tsx
+++ app/spell-analyser/head.tsx
@@ -2,11 +2,11 @@ export default function Head() {
     return (
         <>
             <title>Spell Analyser</title>
-            <meta name="theme-color" content="#" key="theme-color" />
+            <meta name="theme-color" content="#05f721" key="theme-color" />
             <meta property="og:site_name" content="Spell Analyser" key="site-name" />
             <meta property="og:title" content="Spell Analyser" key="title" />
-            <meta property="og:description" content="" key="description" />
-            <meta property="og:image" content="" id="image" />
+            <meta property="og:description" content="This tool was developed to analyse your MagicSpells yaml config, diagnose them, report issues with your config, send tips on how to fix them, and it will even some of them for you automatically for free!" key="description" />
+            <meta property="og:image" content="https://media.discordapp.net/attachments/1079904377571131433/1079904924801957978/image.png" id="image" />
 
             <meta content="width=device-width, initial-scale=1" name="viewport" key="viewport" />
             <link rel="icon" href="/favicon.ico" key="favicon" />
