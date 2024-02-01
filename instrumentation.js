a417f66539a511365e4e7cc8b5a7d456d4bf3b6c
JasperLorelai <wolf.haci12@gmail.com>
2024-02-01 17:28:11 +0100

    feat: Add primitive archive access

--- instrumentation.js
+++ instrumentation.js
@@ -1,9 +1,11 @@
 import fs from "fs";
+import {unzipSync} from "fflate";
 
-const pathDir = "./public";
-const path = pathDir + "/mcVersionData.json";
+const publicDir = "./public";
 
 export async function register() {
+    if (!fs.existsSync(publicDir)) fs.mkdirSync(publicDir);
+
     // Setup Viber
     fetch("https://chatapi.viber.com/pa/set_webhook", {
         method: "POST",
@@ -14,6 +16,23 @@ export async function register() {
         .then(console.log)
         .catch(console.error);
 
+    // Setup archive
+    const archiveZip = "./../../archive/archive.zip";
+    const archiveDir = publicDir + "/archive";
+    if (fs.existsSync(archiveZip)) {
+        if (fs.existsSync(archiveDir)) {
+            fs.rmSync(archiveDir, {force: true, recursive: true, maxRetries: 100});
+        }
+        fs.mkdirSync(archiveDir);
+        const unzipped = unzipSync(new Uint8Array(fs.readFileSync(archiveZip)));
+        for (const [name, contents] of Object.entries(unzipped)) {
+            const isDir = !contents.buffer.byteLength;
+            const path = archiveDir + "/" + name;
+            if (isDir) fs.mkdirSync(path);
+            else fs.writeFileSync(path, Buffer.from(contents.buffer));
+        }
+    }
+
     // Setup MC Version data
     async function save() {
         const versions = await fetch("https://launchermeta.mojang.com/mc/game/version_manifest.json").then(y => y.json());
@@ -22,8 +41,7 @@ export async function register() {
             versionData[type] ??= [];
             versionData[type].push({id, releaseTime});
         }
-        if (!fs.existsSync(pathDir)) fs.mkdirSync(pathDir);
-        fs.writeFileSync(path, JSON.stringify(versionData, null, 4));
+        fs.writeFileSync(publicDir + "/mcVersionData.json", JSON.stringify(versionData, null, 4));
     }
     await save();
     setInterval(save, 24 * 60 * 60 * 1_000);
