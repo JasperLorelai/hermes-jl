90ec254dc2b7a475a8ebe5d6e2492488c27b3882
JasperLorelai <wolf.haci12@gmail.com>
2024-03-16 23:23:10 +0100

    feat: Versioned favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,9 +25,20 @@ export const metadata: Metadata = {
 export default function RootLayout({children}: {children: React.ReactNode}) {
     return (
         <html lang="en" data-bs-theme="dark">
-            <BootstrapJS />
+            <BootstrapJS/>
+            <head>
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=2"/>
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=2"/>
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=2"/>
+                <link rel="manifest" href="/site.webmanifest?v=2"/>
+                <link rel="mask-icon" href="/safari-pinned-tab.svg?v=2" color="#ff0000"/>
+                <link rel="shortcut icon" href="/favicon.ico?v=2"/>
+                <meta name="msapplication-TileColor" content="#000000"/>
+                <meta name="theme-color" content="#ffffff"/>
+                <title>Jasper Lorelai&apos;s website</title>
+            </head>
             <body className={"h-100 cm-scroller text-light bg-black " + font.className}>
-                <main>{children}</main>
+            <main>{children}</main>
             </body>
         </html>
     );
