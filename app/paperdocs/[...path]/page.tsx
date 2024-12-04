5bb73ffffd360fb32b6f67c828bdbcf653778b48
JasperLorelai <wolf.haci12@gmail.com>
2024-12-04 23:11:30 +0100

    feat: Add "Create New paperdocs URL" button

--- app/paperdocs/[...path]/page.tsx
+++ app/paperdocs/[...path]/page.tsx
@@ -4,14 +4,21 @@ import Versions from "./Versions";
 
 export default function Page({params: {path}}: {params: {path: string[]}}) {
     return (
-        <div className="d-flex vh-100 justify-content-center align-items-center">
-            <div>
-                <h1 className="text-decoration-none text-primary">Minecraft Version:</h1>
-                <hr/>
-                <div className="text-center list-group">
-                    <Versions path={path.join("/")} />
+        <>
+            <nav className="navbar navbar-expand-lg bg-primary">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="/paperdocs">Create New URL</a>
+                </div>
+            </nav>
+            <div className="d-flex vh-100 justify-content-center align-items-center">
+                <div>
+                    <h1 className="text-decoration-none text-primary">Minecraft Version:</h1>
+                    <hr/>
+                    <div className="text-center list-group">
+                        <Versions path={path.join("/")}/>
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 }
