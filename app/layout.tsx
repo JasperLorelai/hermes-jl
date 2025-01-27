cf96eb37c4a3dd7a81bb30287d7567d1942693c3
JasperLorelai <wolf.haci12@gmail.com>
2025-01-28 00:02:17 +0100

    feat: Switch from Monocraft to JetBrainsMono font

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,27 @@ import BootstrapJS from "./BootstrapJS";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 
-const font = localFont({src: "../public/Monocraft-no-ligatures.ttf", weight: "300"});
+const font = localFont({
+    src: [
+        {path: "../public/fonts/JetBrainsMono-Thin.woff2", weight: "100", style: "normal"},
+        {path: "../public/fonts/JetBrainsMono-ThinItalic.woff2", weight: "100", style: "italic"},
+        {path: "../public/fonts/JetBrainsMono-ExtraLight.woff2", weight: "200", style: "normal"},
+        {path: "../public/fonts/JetBrainsMono-ExtraLightItalic.woff2", weight: "200", style: "italic"},
+        {path: "../public/fonts/JetBrainsMono-Light.woff2", weight: "300", style: "normal"},
+        {path: "../public/fonts/JetBrainsMono-LightItalic.woff2", weight: "300", style: "italic"},
+        {path: "../public/fonts/JetBrainsMono-Regular.woff2", weight: "400", style: "normal"},
+        {path: "../public/fonts/JetBrainsMono-Italic.woff2", weight: "400", style: "italic"},
+        {path: "../public/fonts/JetBrainsMono-Medium.woff2", weight: "500", style: "normal"},
+        {path: "../public/fonts/JetBrainsMono-MediumItalic.woff2", weight: "500", style: "italic"},
+        {path: "../public/fonts/JetBrainsMono-SemiBold.woff2", weight: "600", style: "normal"},
+        {path: "../public/fonts/JetBrainsMono-SemiBoldItalic.woff2", weight: "600", style: "italic"},
+        {path: "../public/fonts/JetBrainsMono-Bold.woff2", weight: "700", style: "normal"},
+        {path: "../public/fonts/JetBrainsMono-BoldItalic.woff2", weight: "700", style: "italic"},
+        {path: "../public/fonts/JetBrainsMono-ExtraBold.woff2", weight: "800", style: "normal"},
+        {path: "../public/fonts/JetBrainsMono-ExtraBoldItalic.woff2", weight: "800", style: "italic"},
+    ],
+    display: "swap"
+});
 
 import "../styles/general.css";
 
